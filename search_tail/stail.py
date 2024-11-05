c4c0561443231784f65d80bb1ce410650a237fd7
Febin Joy <febinjoy@gmail.com>
2024-11-05 19:25:08 +1100

    Tail the file and print

--- search_tail/stail.py
+++ search_tail/stail.py
@@ -3,8 +3,25 @@ Implementation of Search-Tail
 """
 
 
+def tail(file, lines):
+    """
+    Tails last lines of a file
+    """
+    with open(file, "r", encoding="utf-8") as file_handle:
+        return file_handle.readlines()[-lines:]
+
+
+def print_lines(lines):
+    """
+    Prints lines
+    """
+    for line in lines:
+        print(line, end="")
+
+
 def run(args):
     """
     Main function
     """
-    print(args)
+    lines = tail(args.file, args.n)
+    print_lines(lines)
