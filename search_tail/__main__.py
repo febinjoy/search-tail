cc741ce46fe117e3d798166933a012589b7db426
Febin Joy <febinjoy@gmail.com>
2024-11-05 16:35:00 +1100

    Improve args and add more detailed instructions.

--- search_tail/__main__.py
+++ search_tail/__main__.py
@@ -12,11 +12,33 @@ def main():
     """
     parser = argparse.ArgumentParser(
         description="A tail-like CLI tool with support for search and keyword highlighting",
+        formatter_class=argparse.RawTextHelpFormatter,
     )
-    parser.add_argument("file", type=str, help="The log file to tail")
-    parser.add_argument("-f", "--follow", action="store_true", help="The search query")
     parser.add_argument(
-        "-n", type=int, default=30, help="The number of lines to display"
+        "file",
+        type=str,
+        help="The log file to tail",
+    )
+    parser.add_argument(
+        "-f",
+        "--follow",
+        action="store_true",
+        help=(
+            "Enables the follow mode.\n"
+            "Users will not be able to search when in follow mode.\n"
+            "Keyword highlighting will work."
+        ),
+    )
+    parser.add_argument(
+        "-n",
+        type=int,
+        default=30,
+        help=(
+            "The number of lines to display. Will not follow the file.\n"
+            "While in this mode, use 's' to perform another search.\n"
+            "Use 'n' to navigate to next match and 'p' to navigate to previous match.\n"
+            "Use 'q' to quit the program."
+        ),
     )
     parser.add_argument("-s", type=str, help="Search and highlight a keyword")
     args = parser.parse_args()
